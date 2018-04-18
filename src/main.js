5c1d6e6930a07f0af8815253c3063e34e5428a9e
Jedidiah <yuanzhen_work@163.com>
2018-04-18 15:52:50 +0800

    环境搭建完成

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
+import VueRouter from 'vue-router'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,18 @@
 // @flow
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import StudyDocLists from '@/components/StudyDocLists/StudyDocLists'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'StudyDocLists',
+      component: StudyDocLists
     }
   ]
 })
